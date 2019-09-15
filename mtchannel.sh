978a27a24715c07a84112fe02baa06a3b722f574
S1S4NTUY <55349366+S1S4NTUY@users.noreply.github.com>
2019-09-15 18:38:26 +0700

    Add files via upload

---
+++ mtchannel.sh
@@ -0,0 +1,252 @@
+#!/bin/bash
+clear
+r='\033[31;2m' #merah
+g='\033[32;1m' #hijau
+b='\033[34;2m' #biru
+p='\033[35;1m' #purple
+cy='\033[36;2m' #cyan
+w='\033[37;1m' #putih
+y='\033[33;1m' #kuning
+
+# [Coded By] Rizki18, Youtube : Mhank Tuttorr
+
+echo $r""
+figlet -f standard MT-Channel
+echo
+sleep 1
+echo $p"["$p"═════════════════════════════════════"$p"]"
+echo $p"["$r"=>"$p"]"$r" Author: Rizki Santuy"   
+echo $p"["$r"=>"$p"]"$r" Contact Me: 0895361164306"   
+echo $p"["$r"=>"$p"]"$r" Youtube: Mhank Tuttorr "$p"SUBSCRIBE"
+echo $p"["$r"=>"$p"]"$r" Indonesian DarkNet Team"
+echo $p"["$p"═════════════════════════════════════"$p"]"
+echo
+sleep 2
+echo $p"["$cy"1"$p"]"$b" Hiburan dulu Yok.."
+sleep 1
+echo $p"["$cy"2"$p"]"$b" Ping Checker"$p" v1"
+sleep 1
+echo $p"["$cy"3"$p"]"$b" Hack Facebook"$p" PREMIUM"
+sleep 1
+echo $p"["$cy"4"$p"]"$b" Installer TN. B4DUT"
+sleep 1
+echo $p"["$cy"5"$p"]"$b" TOOLS DDOS"
+sleep 1
+echo $p"["$cy"6"$p"]"$b" Sadap Whatsapp V.Dog"
+sleep 1
+echo $p"["$cy"7"$p"]"$b" Sinyal Stabilizer"
+sleep 1
+echo $p"["$cy"★"$p"]"$b" Coming Soon..."$p"["$r"^"$b"_"$r"^"$p"]"
+sleep 1
+echo $p"║"
+read -p"╚═[Pilih Sesuai Kebutuhan]═➢" mt
+
+if [ $mt = 1 ]
+then
+clear
+echo $p""
+figlet -f slant "WAIT..."
+sleep 1
+echo $p"["$w"+"$p"]" $w"Sedang Mencari Ruang karoke"
+sleep 1
+echo $p"["$w"+"$p"]" $w"Menuju tak terbatas dan melampaui nya"
+sleep 1
+echo $p"["$w"+"$p"]" $w"..."
+sleep 1
+clear
+sleep 1
+echo $p"Termux Karouke" 
+echo $w"Lirik lagu Lihat kebunku versi Mod"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Lihat Pacarku"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Dia main gila"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Punya kekasih"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Kok tidak setia"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Setiap hari"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Ku doakan dia"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Semoga mati"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Keseruduk beca !"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Beca, beca !"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" coba bawa saya"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" saya mau tamasya"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" berkliling keliling kota"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Hendak melihat lihat"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Kegiatan si doi"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Lihat beca ku lari !"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Kereta tak berhenti"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Beca, beca !"
+echo
+sleep 1
+echo $p"["$w""$p"]" $w" Tabrak doi lagi ! :("
+echo
+sleep 1
+echo $p"Terimakasih"$w" Atas kegabutan kalian"
+echo $w"            Yang sudah menggunakan tools unfaedah ini"
+echo $p"Dasar"
+sleep 1
+echo $w"Gabut ..."
+echo $p"Dont Forget To Subscribe Channel Mhank Tuttorr"
+echo $w"WAJIB"
+sleep 3
+sh mtchannel.sh
+fi
+
+if [ $mt = 2 ]
+then
+clear
+echo $p""
+figlet -f standard MT-Channel
+echo
+echo $r"["$p"═════════════════════════════════════"$r"]"
+echo $r"["$p"+"$r"]"$p" Dibuat khusus Pengguna Kartu Rakyat"
+echo $r"["$p"+"$r"]"$p" Dilarang menggunakan tools ini"
+echo $r"["$p"+"$r"]"$p" Kecuali Kartu "$r"Rakyat ..."
+echo $r"["$p"═════════════════════════════════════"$r"]"
+echo
+sleep 2
+clear
+echo $r""
+figlet -f slant "WAIT..."
+sleep 2
+echo $r"["$w"+"$r"]" $p"Sedang Mencari Server Terbaik"
+sleep 2
+echo $r"["$w"+"$r"]" $p"Menuju tak terbatas dan melampaui nya"
+sleep 2
+echo $r"["$w"+"$r"]" $p"Connecting..."
+sleep 1
+ping google.com
+fi
+
+if [ $mt = 3 ]
+then
+clear
+echo $p""
+figlet -f standard MT-Channel
+echo
+echo $p"["$w"═════════════════════════════════════"$p"]"
+echo $p"["$w"+"$p"]"$w" Author: Mhank Tuttorr"
+echo $p"["$w"+"$p"]"$w" Youtube: Mhank Tuttorr"
+echo $p"["$w"+"$p"]"$w" DONT FORGET TO "$p"SUBSCRIBE ..."
+echo $p"["$w"+"$p"]"$w" Script Installer Dark-Fb v1.8"
+echo $p"["$w"═════════════════════════════════════"$p"]"
+echo
+sleep 2
+echo $p"["$w"1"$p"]"$w" Mencoba Untuk Memasang.."$p" Facebook PREMIUM"
+echo $r"" 
+figlet -f slant "Wait..."
+sleep 2
+pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;python2 fb.py
+fi
+
+if [ $mt = 4 ]
+then
+clear
+echo $p""
+figlet -f standard MT-Channel
+echo
+echo $p"["$w"═════════════════════════════════════"$p"]"
+echo $p"["$w"+"$p"]"$w" Script Installer Tuan B4DUT"
+echo $p"["$w"+"$p"]"$w" Dibuat khusus untuk Hacker pro"
+echo $p"["$w"+"$p"]"$w" Dilarang menggunakan tools ini"
+echo $p"["$w"+"$p"]"$w" Kecuali lagi "$p"gabut ..."
+echo $p"["$w"═════════════════════════════════════"$p"]"
+echo
+sleep 2 
+echo $p"["$w"1"$p"]"$w" Mencoba Untuk Memasang..."
+echo $r"" 
+figlet -f slant "Wait..."
+sleep 2
+apt update;apt upgrade -y;apt install git;apt install php;apt install curl;apt install ruby;apt install figlet;apt install python2;gem install lolcat;git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv3;cd TOOLSINSTALLERv3;chmod +x TUANB4DUT.sh;sh TUANB4DUT.sh
+fi
+
+if [ $mt = 5 ]
+then 
+clear
+echo $p""
+figlet -f standard MT-Channel
+echo
+echo $p"<"$r"===]==========================================>"
+echo $p"["$r"+"$w" Installer Script DDOS"
+echo $p"<"$r"==========================================[===>"
+echo
+echo $p"=>"$p"Cara Penggunaan: python Tusbol.py -s (IP adress web) -p 80 -t 135 Cara mencari alamat ip web: ping (nama web) Contoh: ping www.target.com"
+sleep 6
+clear
+figlet -f slant "wait..."
+sleep 1
+pkg install python;pkg install git;pip2 install requsts;pip2 install mechanize;git clone https://github.com/ToxicCyberTeam/DDOS;cd DDOS;python Tusbol.py
+fi
+
+if [ $mt = 6 ]
+then
+clear
+echo $p"["$r"+"$p" Sadap Whatsapp V.Dog"
+sleep1
+sh wa.sh
+fi
+
+if [ $mt = 7 ]
+then
+clear
+echo $p""
+figlet -f standard MT-Channel
+echo
+echo $p"["$w"═════════════════════════════════════"$p"]"
+echo $p"["$w"+"$p"]"$w" Script Installer Sinyal Stabilizer"
+echo $p"["$w"+"$p"]"$w" Dibuat khusus untuk Kartu Rakyat"
+echo $p"["$w"+"$p"]"$w" Dilarang menggunakan tools ini"
+echo $p"["$w"+"$p"]"$w" Kecuali lagi "$p"Kartu Rakyat ..."
+echo $p"["$w"═════════════════════════════════════"$p"]"
+echo
+sleep 2
+echo $p"["$w"1"$p"]"$w" Mencoba Untuk Menstabilkan..."
+echo $r""
+figlet -f slant "Wait..."
+sleep 2
+ping 8.8.8.8
+fi
+
+if [ $mt = ★ ]
+then
+clear
+echo $p""
+figlet -f standard MT-Channel
+echo
+sleep 1
+echo $r "Coming Soon Anjing..."
+sleep 4
+sh mtchannel.sh
+fi
